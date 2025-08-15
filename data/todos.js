91d80318ed899429193c47cfeed1afb89ed9d965
Akash Kumar <166211035+akash8979thakur@users.noreply.github.com>
2025-08-15 23:09:06 +0530

    create CrudeApp

---
+++ data/todos.js
@@ -0,0 +1,92 @@
+export const data = [
+    {
+        "id": 1,
+        "title": "Buy groceries",
+        "completed": false
+    },
+    {
+        "id": 2,
+        "title": "Walk the dog",
+        "completed": true
+    },
+    {
+        "id": 3,
+        "title": "Read a book",
+        "completed": false
+    },
+    {
+        "id": 4,
+        "title": "Write code",
+        "completed": true
+    },
+    {
+        "id": 5,
+        "title": "Exercise",
+        "completed": false
+    },
+    {
+        "id": 6,
+        "title": "coding",
+        "completed": true
+    },
+     {
+        "id": 7,
+        "title": "Buy groceries",
+        "completed": false
+    },
+    {
+        "id": 8,
+        "title": "Walk the dog",
+        "completed": true
+    },
+    {
+        "id": 9,
+        "title": "Read a book",
+        "completed": false
+    },
+    {
+        "id": 10,
+        "title": "Write code",
+        "completed": true
+    },
+    {
+        "id": 11,
+        "title": "Exercise",
+        "completed": false
+    },
+    {
+        "id": 12,
+        "title": "coding",
+        "completed": true
+    },
+     {
+        "id": 13,
+        "title": "Buy groceries",
+        "completed": false
+    },
+    {
+        "id": 14,
+        "title": "Walk the dog",
+        "completed": true
+    },
+    {
+        "id": 15,
+        "title": "Read a book",
+        "completed": false
+    },
+    {
+        "id": 16,
+        "title": "Write code",
+        "completed": true
+    },
+    {
+        "id": 17,
+        "title": "Exercise",
+        "completed": false
+    },
+    {
+        "id": 18,
+        "title": "coding",
+        "completed": true
+    }
+]
